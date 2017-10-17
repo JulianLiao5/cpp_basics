c0bd64465802ca0492c038167d6e1ed334fd1e4b
LiaoMeng <meng.liao@perceptin.io>
2017-10-17 11:28:08 +0800

    Adds more test case for high_resolution_clock.
     Changes to be committed:
    	modified:   test_high_resolution_clock.cpp

--- test_high_resolution_clock.cpp
+++ test_high_resolution_clock.cpp
@@ -6,7 +6,7 @@
  ************************************************************************/
 
 #include<iostream>
-
+#include <stdio.h>
 #include <chrono>
 #include <thread>
 
@@ -18,6 +18,13 @@ int main(int argc, char *argv[]) {
     using namespace std::chrono;
 
     std::cout<<"*****************test_case_1****************"<<std::endl;
+    high_resolution_clock::time_point now_tp = high_resolution_clock::now();
+    long long now_l = static_cast<long long>(duration_cast<std::chrono::milliseconds>(now_tp.time_since_epoch()).count());
+    printf("now_d: %lld\n", now_l);
+
+    std::cout<<"*****************test_case_2****************"<<std::endl;
+
+    std::cout<<"*****************test_case_3****************"<<std::endl;
 
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
@@ -33,7 +40,7 @@ int main(int argc, char *argv[]) {
 
     std::cout<<"It took me "<<time_span.count()<<" seconds."<<std::endl;
 
-    std::cout<<"*****************test_case_2****************"<<std::endl;
+    std::cout<<"*****************test_case_4****************"<<std::endl;
     high_resolution_clock::time_point start = high_resolution_clock::now();
     std::this_thread::sleep_for(std::chrono::seconds(2));
     high_resolution_clock::time_point end = high_resolution_clock::now();

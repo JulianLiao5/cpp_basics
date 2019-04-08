8a654b85966024372943edcdef3c9c72e19e495c
LiaoMeng <meng.liao@perceptin.io>
2019-04-08 15:02:52 +0800

    Adds test vector cases.
     Changes to be committed:
    	modified:   data_struct/CMakeLists.txt
    	modified:   data_struct/test_vector.cc

--- data_struct/test_vector.cc
+++ data_struct/test_vector.cc
@@ -1,4 +1,5 @@
 
+#include <climits>
 #include <iostream>
 #include <vector>
 
@@ -12,6 +13,19 @@ struct PntIdx {
 };
 
 int main(int argc, char** argv) {
+    unsigned int a;
+    printf("sizeof(unsigned int)    :    %lu\n", sizeof(unsigned int));
+    printf("sizeof(unsigned int a)    :    %lu\n", sizeof(a));
+
+    printf("UINT_MAX    :    %u\n", UINT_MAX);
+    a = UINT_MAX - 1;
+    printf("11 - a    :    %u\n", a);
+    a += 1;
+    printf("22 - a    :    %u\n", a);
+    a += 1;
+    printf("33 - a    :    %u\n", a);
+
+
 
     std::vector<PntIdx> linePtIdx;
     linePtIdx.push_back(PntIdx(714, 810));
@@ -29,8 +43,8 @@ int main(int argc, char** argv) {
     linePtIdx.push_back(PntIdx(706, 822));
     linePtIdx.push_back(PntIdx(705, 823));
 
-    int a = linePtIdx.end() - linePtIdx.begin();
-    std::cout << "a: " << a << ",  size: " << linePtIdx.size() << std::endl;
+    int size_a = linePtIdx.end() - linePtIdx.begin();
+    std::cout << "size_a: " << size_a << ",  size: " << linePtIdx.size() << std::endl;
 
     return 0;
 }

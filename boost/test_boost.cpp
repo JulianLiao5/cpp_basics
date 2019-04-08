308178f71db0fd548cdbe1d82ece9b1ea478a444
LiaoMeng <meng.liao@perceptin.io>
2019-04-08 15:34:04 +0800

    1. Google C++ Style.
     Changes to be committed:
    	modified:   .gitignore
    	modified:   boost/test_boost.cpp

--- boost/test_boost.cpp
+++ boost/test_boost.cpp
@@ -1,33 +1,33 @@
 /*************************************************************************
-	> File Name: test_boost.cpp
-	> Author: 
-	> Mail: 
-	> Created Time: 2019年04月08日 星期一 15时04分52秒
+        > File Name: test_boost.cpp
+        > Author:
+        > Mail:
+        > Created Time: 2019年04月08日 星期一 15时04分52秒
  ************************************************************************/
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 #include <boost/signals2/signal.hpp>
 
 struct HelloWorld {
-    void operator()() const;
+  void operator()() const;
 };
 
 void HelloWorld::operator()() const {
-        std::cout << "Hello, liaomeng!" << std::endl;
+  std::cout << "Hello, liaomeng!" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
   // Signal with no arguments and a void return value
-  boost::signals2::signal<void ()> sig;
-    
+  boost::signals2::signal<void()> sig;
+
   // Connect a HelloWorld slot
   HelloWorld hello;
-    sig.connect(hello);
+  sig.connect(hello);
 
-    // Call all of the slots
-    sig();
+  // Call all of the slots
+  sig();
 
-    return 0;
+  return 0;
 }

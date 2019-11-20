726d696c7c288d2ab80ab1e6a9b97232d09a392f
Meng Liao <meng.liao@perceptin.io>
2019-11-20 12:27:28 +0800

    我掌握了用Eigen根据vehicle_coordinate的3 * 1矩阵去求解radar raw data Range/Azimuth的办法了。
     Changes to be committed:
    	new file:   knowledge.md
    	modified:   src/test_eigen_matrix.cpp

--- eigen/src/test_eigen_matrix.cpp
+++ eigen/src/test_eigen_matrix.cpp
@@ -11,6 +11,7 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 
+using namespace Eigen;
 using namespace std;
 
 
@@ -48,12 +49,20 @@ int main(int argc, char** argv) {
     v_3d << 3, 2, 1;
     vd_3d << 4, 5, 6;
 
+    // [1    2    3]    [3]    [10]
+    //                  [2]  =
+    // [4    5    6]    [1]    [28]
     // 先来个错误的吧，在Eigen里，你不能混合两种不同类型的矩阵
     // Eigen::Matrix<double, 2, 1> result_1 = matrix_23 * v_3d;
     // cout << result_1 << endl;
     Eigen::Matrix<double, 2, 1> result_2 = matrix_23.cast<double>() * v_3d;
     cout << result_2 << endl;
 
+    cout << "------------------" << endl;
+
+    // [1    2    3]    [4]    [32]
+    //                  [5]  =
+    // [4    5    6]    [6]    [77]
     Eigen::Matrix<float, 2, 1> result_3 = matrix_23 * vd_3d;
     cout << result_3 << endl;
 
@@ -64,9 +73,24 @@ int main(int argc, char** argv) {
       3, 4;
     Eigen::MatrixXd B = A.transpose();  // the transpose of A is a 2*3 matrix
 
+    //             [1    2]     [1    2    3]     [ 5     8    11]
+    // A * B =     [2    3]  *                 =  [ 8    13    18]
+    //             [3    4]     [2    3    4]     [11    18    25]
+    //----------------------------------------------------------------
+    //             [1    2    3]     [1    2]     [14    20]
+    // B * A =                    *  [2    3]  =
+    //             [2    3    4]     [3    4]     [20    29]
     Eigen::MatrixXd C = B * A;
     Eigen::MatrixXd D = (B * A).inverse();
+    Eigen::MatrixXd G = C.inverse();
 
+    cout << "G--[BEGIN]" << endl;
+    cout << G << endl;
+    cout << "G--[END]" << endl;
+
+    cout << "A--[BEGIN]" << endl;
+    cout << A << endl;
+    cout << "A--[END]" << endl;
 
     cout << "B--[BEGIN]" << endl;
     cout << B << endl;
@@ -79,6 +103,40 @@ int main(int argc, char** argv) {
     cout << "D--[BEGIN]" << endl;
     cout << D << endl;
     cout << "D--[END]" << endl;
+
+    // ----------------BEGIN basic linear solving-----------------------
+    // 当A2, b2, x2的type分别是Matrix3f, Vector3f, Vector3f时，结果不准确
+    // The solution(x2) is:
+    //       -2
+    //        0.999997
+    //        1
+    // 当A2, b2, x2的type分别是Matrix3d, Vector3d, Vector3d时，结果准确
+    // The solution(x2) is:
+    //       -2
+    //        1
+    //        1
+    //
+    Matrix3d A2;
+    Vector3d b2;
+    A2 << 1,2,3,    4,5,6,    7,8,10;
+    b2 << 3,3,4;
+    cout << "A2:\n" << A2 << "\n\n";
+    cout << "b2:\n" << b2 << "\n\n";
+    Vector3d x2 = A2.colPivHouseholderQr().solve(b2);
+    cout << "The solution(x2) is:\n" << x2 << endl;
+
+    A2 << 0, -1,  0.935,    1, 0, 0.77,    0,0,1;
+    b2 << 0.762372, 1.17472, 1;
+    cout << "A2:\n" << A2 << "\n\n";
+    cout << "b2:\n" << b2 << "\n\n";
+    Vector3d x3 = A2.colPivHouseholderQr().solve(b2);
+    cout << "The solution(x3) is:\n" << x3 << endl;
+
+    double Azimuth = atan2(-x3[1], x3[0]) * 180 / M_PI;
+    double Range = x3[0] / cos(Azimuth * M_PI / 180);
+    cout << "\nRange: " << Range << ", Azimuth: " << Azimuth << "\n";
+
+    // ----------------END basic linear solving-----------------------
 
     Eigen::Quaterniond q(2, 0, 1, -3);
     cout << "This quaternion consists of a scalar " << q.w() << " and a vector " << endl << q.vec() << endl;
@@ -100,6 +158,10 @@ int main(int argc, char** argv) {
     cout << R << endl;
     cout << "R--[END]" << endl;
     cout << "Compare with the result using an rotation matrix " << endl << (R * v) << std::endl;
+
+    cout << "the sum of each column of R: " << R.colwise().sum() << "\n\n";
+    cout << "the maximum value of each column of R: " << R.colwise().maxCoeff() << "\n\n";
+    cout << "the maximum absolute value of each column of R: " << R.cwiseAbs().colwise().maxCoeff() << "\n\n";
 
     Eigen::Vector3d euler = q.toRotationMatrix().eulerAngles(0, 1, 2);
     cout << "Euler from quaternion in roll, pitch, yaw: " << endl << euler << endl;

0a28d2566574c834026f49461ed5add0f88a08ff
LiaoMeng <meng.liao@perceptin.io>
2020-02-11 17:06:05 +0800

    添加对char, signed char, unsigned char的理解。
     Changes to be committed:
    	modified:   README.md
    	modified:   data_struct/test_data_struct.cc

--- data_struct/test_data_struct.cc
+++ data_struct/test_data_struct.cc
@@ -2,24 +2,39 @@
 #include <stdio.h>
 #include <string>
 
+#include <iostream>
+#include <limits.h>
 
 int main(int argc, char *argv[]) {
+
+  /**------------------BEGIN char-----------------------**/
+  /**
+  char cc2;
+  signed char sc1;
+  unsigned char usc1;
+  printf("line: %d, char_size: %ld, signed_char_size: %ld, unsigned_char_size: %ld\n", __LINE__, sizeof(cc2), sizeof(sc1), sizeof(usc1));
+  std::cout << __LINE__ << "  min_signed_char: " << SCHAR_MIN << ", max_signed_char: " << SCHAR_MAX << std::endl;
+  std::cout << __LINE__ << "  min_char: " << CHAR_MIN << ", max_char: " << CHAR_MAX << std::endl;
+  std::cout << __LINE__ << "  max_unsigned_char: " << UCHAR_MAX << std::endl;
+  **/
+  /**------------------END char-----------------------**/
   int aa;
   unsigned int bb;
   short int cc;
   short dd;
   uint8_t a1;
   int8_t a2;
-    uint16_t b1;
-    int16_t b2;
-    uint32_t c1;
-    int32_t c2;
-    uint64_t d1;
-    int64_t d2;
+  uint16_t b1;
+  int16_t b2;
+  uint32_t c1;
+  int32_t c2;
+  uint64_t d1;
+  int64_t d2;
   printf("int_size: %ld, unsigned int size: %ld, short_int_size: %ld, short size: %ld\n", sizeof(aa), sizeof(bb), sizeof(cc), sizeof(dd));
 
   printf("uint8_t_size: %ld, int8_t size: %ld, uint16_t_size: %ld, int16_t size: %ld\n", sizeof(a1), sizeof(a2), sizeof(b1), sizeof(b2));
   printf("uint32_t_size: %ld, int32_t size: %ld, uint64_t_size: %ld, int64_t size: %ld\n", sizeof(c1), sizeof(c2), sizeof(d1), sizeof(d2));
+
 /**
   printf("min_int8_t: %d, max_int8_t: %d\n", MIN_OF(int8_t), MAX_OF(int8_t));
   printf("min_int16_t: %d, max_int16_t: %d\n", MIN_OF(int16_t), MAX_OF(int16_t));
